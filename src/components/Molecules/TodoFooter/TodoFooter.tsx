5aa260032187b17d49db1bc29a9092e1b2580b36
Matheus F.Corocher <matheuscorocher@gmail.com>
2022-09-12 15:19:59 -0300

    refactor: change button to list item

--- src/components/Molecules/TodoFooter/TodoFooter.tsx
+++ src/components/Molecules/TodoFooter/TodoFooter.tsx
@@ -7,7 +7,7 @@ interface TodoFooter {
 }
 
 function TodoFooter({}: TodoFooter) {
-    const handleClick = (event : React.MouseEvent<HTMLButtonElement, MouseEvent>) : void => {
+    const handleClick = (event : React.MouseEvent<HTMLAnchorElement, MouseEvent>) : void => {
         document.querySelectorAll('.active').forEach(e => e.classList.remove('active'));
         event.currentTarget.classList.toggle('active')
     }
@@ -18,11 +18,11 @@ function TodoFooter({}: TodoFooter) {
                 <strong>1&nbsp;</strong>
                 item left
             </span>
-            <div className={`todo-filters`}>
-                <button className={'todo-filter'} onClick={handleClick} >All</button>
-                <button className={'todo-filter'} onClick={handleClick} >Active</button>
-                <button className={'todo-filter'} onClick={handleClick} >Completed</button>
-            </div>
+            <ul className={`todo-filters`}>
+                <li><a className={'todo-filter'} onClick={handleClick} href="#/">All</a></li>
+                <li><a className={'todo-filter'} onClick={handleClick} href="#/active">Active</a></li>
+                <li><a className={'todo-filter'} onClick={handleClick} href="#/completed">Completed</a></li>
+            </ul>
         </div>
     );
 }
