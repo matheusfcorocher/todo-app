139327f3fed7f910a243904876be436f19b7fc7e
Matheus F.Corocher <matheuscorocher@gmail.com>
2022-09-13 10:10:37 -0300

    refactor: refact Todo component and now has a initial state

--- src/components/Organisms/Todo/Todo.tsx
+++ src/components/Organisms/Todo/Todo.tsx
@@ -4,6 +4,9 @@ import '../../../App.css';
 import './todolist.css';
 import list from '../../../assets/icons/list-icon.svg';
 import checkList from '../../../assets/icons/check-list-icon.svg';
+import TodoMenu from '../../Molecules/TodoMenu/TodoMenu';
+import TodoList from '../../Molecules/TodoList/TodoList';
+import TodoFooter from '../../Molecules/TodoFooter/TodoFooter';
 
 type TodoData = {
   id: string;
@@ -16,60 +19,14 @@ interface TodoProps {
 }
 
 function Todo({ todosData = [] }: TodoProps) {
-  const [todos, setTodos] = useState([...todosData]);
-
-  const handleCreateTodoItem = (TodoTitle: string) => {
-
-  };
-
-  const listToDoItems = todos.map((todo: TodoData) => {
-    return (
-      <TodoItem
-        key={todo.id}
-        task={{
-          id: todo.id,
-          title: todo.title,
-          isCompleted: todo.isCompleted
-        }}
-        handleUpdateTodoItemTitle={function (newTitle: string): void {
-          throw new Error('Function not implemented.');
-        }}
-
-        handleUpdateTodoItemState={function (newState: boolean): void {
-          throw new Error('Function not implemented.');
-        }}
-        handleDeleteTodoItem={function (newState: string): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
-    );
-  }
-  );
-
   return (
     <div className={"todo-panel"}>
-      <div className={`todo-menu`}>
-        <button className={`complete-button`} >
-          <img 
-            src={checkList} 
-            className="check-icon" 
-            alt="check-all-icon" 
-          />
-          {/* <img src={list} className="uncheck-icon" alt="uncheck-all-icon" /> */}
-        </button>
-        <input
-          type="text"
-          role="textbox"
-          placeholder='What needs to be done?'
-          value={''}
-          name="title"
-          className={`title-input`}
-          onChange={() => handleCreateTodoItem}
-        />
-      </div>
-      <ul className={"todo-list"}>
-        {listToDoItems}
-      </ul>
+      <TodoMenu handleCreateTodo={function (): void {
+        throw new Error('Function not implemented.');
+      } } isAllTodosCompleted={false}      
+      />
+      <TodoList todosData={todosData} />
+      <TodoFooter />
     </div>
   );
 }
