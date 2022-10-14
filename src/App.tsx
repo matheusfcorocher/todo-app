cfa5a48eaee77a3b481f99e3a220590529b2f3fb
Matheus F.Corocher <matheuscorocher@gmail.com>
2022-10-14 13:02:56 -0300

    refactor: remove construction of Use Cases in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,7 @@ import './App.css';
 import Todo from './view/components/Organisms/Todo/Todo';
 import { TodoTasks } from './domain/entities/TodoTask';
 import { lsTodoTaskRepository } from './infra/repositories/LSTodoTaskRepository';
-import { makeAddTodoTask } from './app/TodoTask/AddTodoTask/AddTodoTask';
-import { makeDeleteTodoTask } from './app/TodoTask/DeleteTodoTask/DeleteTodoTask';
-import { makeUpdateTodoTaskTitle } from './app/TodoTask/UpdateTodoTaskTitle/UpdateTodoTaskTitle';
-import { makeUpdateTodoTaskState } from './app/TodoTask/UpdateTodoTaskState/UpdateTodoTaskState';
-import { makeCompleteAllTodoTasks } from './app/TodoTask/CompleteAllTodoTasks/CompleteAllTodoTask';
-import { makeIncompleteAllTodoTasks } from './app/TodoTask/IncompleteAllTodoTasks/IncompleteAllTodoTask';
-import { makeDeleteAllCompletedTodoTasks } from './app/TodoTask/DeleteAllCompletedTodoTasks/DeleteAllCompletedTodoTasks';
+import { addTodoTask, completeAllTodoTasks, deleteAllCompletedTodoTasks, deleteTodoTask, incompleteAllTodoTasks, updateTodoTaskState, updateTodoTaskTitle } from './view/container';
 
 export type HandleCreateTodo = (title: string) => void;
 export type HandleDeleteTodo = (id: string) => void;
@@ -25,25 +19,21 @@ function App() {
   const [filter, setFilter] = useState<boolean | undefined>();
 
   function handleAddTodo(title: string): void {
-    const addTodoTask = makeAddTodoTask(lsTodoTaskRepository);
     const newTodoTasks = addTodoTask({todoTasks, title});
     setTodoTasks(newTodoTasks)
   }
 
   function handleDeleteTodo(id: string): void {
-    const deleteTodoTask = makeDeleteTodoTask(lsTodoTaskRepository);
     const newTodoTasks = deleteTodoTask({todoTasks, id});
     setTodoTasks(newTodoTasks);
   }
 
   function handleUpdateTodoTitle(id: string, newTitle: string): void {
-    const updateTodoTaskTitle = makeUpdateTodoTaskTitle(lsTodoTaskRepository);
     const newTodoTasks = updateTodoTaskTitle({todoTasks, id, newTitle});
     setTodoTasks(newTodoTasks);
   }
 
   function handleUpdateTodoState(id: string, state: boolean): void {
-    const updateTodoTaskState = makeUpdateTodoTaskState(lsTodoTaskRepository);
     const newTodoTasks = updateTodoTaskState({todoTasks, id, state});
     setTodoTasks(newTodoTasks);
   }
@@ -56,13 +46,11 @@ function App() {
   // } 
 
   function handleCompleteAllTodoTasksItem(): void {
-    const completeAllTodoTasks = makeCompleteAllTodoTasks(lsTodoTaskRepository);
     const newTodoTasks = completeAllTodoTasks({todoTasks});
     setTodoTasks(newTodoTasks);
   }
 
   function handleIncompleteAllTodoTasks(): void {
-    const incompleteAllTodoTasks = makeIncompleteAllTodoTasks(lsTodoTaskRepository);
     const newTodoTasks = incompleteAllTodoTasks({todoTasks});
     setTodoTasks(newTodoTasks);
   }
@@ -72,7 +60,6 @@ function App() {
   }
 
   function handleDeleteAllCompletedTodoTasks(): void {
-    const deleteAllCompletedTodoTasks = makeDeleteAllCompletedTodoTasks(lsTodoTaskRepository);
     const newTodoTasks = deleteAllCompletedTodoTasks({todoTasks});
     setTodoTasks(newTodoTasks)
   }
