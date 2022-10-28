3d318c2263b3fc17fb66ac0353f47d6dbb524b97
matheusfc <matheuscorocher@gmail.com>
2022-10-28 16:28:46 -0300

    feat: add animation to TodoList using react-spring

--- src/presentation/view/components/Molecules/TodoList/TodoList.tsx
+++ src/presentation/view/components/Molecules/TodoList/TodoList.tsx
@@ -1,7 +1,6 @@
+import { animated, useTransition } from "@react-spring/web";
 import React, { useState } from "react";
-import {
-  filterTodoTasksByIsCompleted,
-} from "../../../../../domain/entities/TodoTask";
+import { filterTodoTasksByIsCompleted, TodoTask } from "../../../../../domain/entities/TodoTask";
 import { IsCompletedFilterControllerReturnType } from "../../../../controllers/IsCompletedFilterController";
 import { TodoTaskControllerReturnType } from "../../../../controllers/TodoTaskController";
 import { TodoItem } from "../TodoItem/TodoItem";
@@ -12,32 +11,47 @@ interface TodoListProps {
   todoTaskController: TodoTaskControllerReturnType;
 }
 
-function TodoList({ isCompletedFilterController, todoTaskController }: TodoListProps) {
+function TodoList({
+  isCompletedFilterController,
+  todoTaskController,
+}: TodoListProps) {
   const { getTodoTasks } = todoTaskController;
   const todoTasks = getTodoTasks();
   const { getIsCompletedFilter } = isCompletedFilterController;
   const filter = getIsCompletedFilter();
-  
+
   const listToDoItems = filterTodoTasksByIsCompleted({
     todoTasks,
     isCompleted: filter,
-  }).map((todo) => {
-    return (
-      <TodoItem
-        key={todo.id}
-        task={{
-          id: todo.id,
-          title: todo.title,
-          isCompleted: todo.isCompleted,
-        }}
-        todoTaskController={todoTaskController}
-      />
-    );
   });
 
+
+  //Animations
+  const transitions = useTransition(
+    listToDoItems,
+    {
+      key: (item: TodoTask) => item.id,
+      from: { opacity: 0, x: 0 },
+      leave: { opacity: 0, x: 50 },
+      enter: { opacity: 1 }, 
+    }
+  );
+  
   return (
     <ul role="list" className={"todo-list"}>
-      {listToDoItems}
+      {transitions((style, item) => (
+        <animated.div style={style}>
+          <TodoItem
+            key={item.id}
+            task={{
+              id: item.id,
+              title: item.title,
+              isCompleted: item.isCompleted,
+            }}
+            todoTaskController={todoTaskController}
+          />
+        </animated.div>
+      ))}
     </ul>
   );
 }
